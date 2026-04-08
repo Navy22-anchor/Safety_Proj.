8e4a5bf024cfb4ed458440e628a6658ae53ad4c4
Navy22-anchor <lss4583@gmail.com>
2026-04-08 14:13:06 +0900

    Assemble Comp : ChatPages

---
+++ ReFact._Safety_Proj./src/Pages/Page_chat/ChatPage.tsx
@@ -0,0 +1,11 @@
+import {Chat_header,Chat_messages,Chat_footer} from '../../components/ChatBot/ChatBot'
+const ChatPage = () => {
+    return (
+        <>
+            <Chat_header/>
+            <Chat_messages/>
+            <Chat_footer/>
+        </>
+    )
+}
+export default ChatPage
