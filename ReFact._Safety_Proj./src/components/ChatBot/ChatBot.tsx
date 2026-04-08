2933b92ebaa99a864615f538e24103d25154fe2a
Navy22-anchor <lss4583@gmail.com>
2026-04-08 14:04:40 +0900

    New Comp : Chat-Footer

--- ReFact._Safety_Proj./src/components/ChatBot/ChatBot.tsx
+++ ReFact._Safety_Proj./src/components/ChatBot/ChatBot.tsx
@@ -1,4 +1,3 @@
-
 const chat_header = () => {
     return (
         <>
@@ -16,7 +15,6 @@ const chat_header = () => {
         </>
     )
 }
-
 const Chat_messages = () => {
     return (
         <>
@@ -24,5 +22,25 @@ const Chat_messages = () => {
         </>
     )
 }
-
-export default {chat_header}
+const Chat_footer = () => {
+    return (
+        <>
+            <footer className="chat-input-area">
+            <div className="input-wrapper">
+                <div className="suggestion-btn-area">
+                    <button id="suggestion-guide-btn" className="suggestion-btn">위험성 질문 가이드</button>
+                    </div>
+                <div className="input-field-wrapper">
+                    <input type="text" id="chat-input" placeholder="궁금한 내용을 입력하세요."></input>
+                    <button id="send-btn" className="send-btn">
+                        <svg viewBox="0 0 24 24" width="24" height="24">
+                            <path fill="currentColor" d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </footer>
+        </>
+    )
+}
+export default {chat_header,Chat_messages,Chat_footer}
